5ac2e61340e389237e0a9aecb1a77e8776411406
sunshinezhy <zhy2539@126.com>
2021-02-17 21:20:26 +0800

    完成前端登录注册相关界面

---
+++ book-mgr-fe/src/views/Auth/index.js
@@ -0,0 +1,14 @@
+import { formatCountdown } from 'ant-design-vue/lib/statistic/utils';
+import { defineComponent } from 'vue';
+import { UserOutlined, LockOutlined, LinkOutlined } from '@ant-design/icons-vue';
+
+export default defineComponent({
+    components:{
+        UserOutlined,
+        LockOutlined,
+        LinkOutlined,
+    },
+    setup(){
+
+    },
+});
